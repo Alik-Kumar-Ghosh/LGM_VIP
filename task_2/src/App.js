d32221a1b183a850912b3129d7a579edbe3c51a6
Alik Kumar Ghosh <33929979+Alik-Kumar-Ghosh@users.noreply.github.com>
2021-07-24 18:58:26 +0530

    Add files via upload

---
+++ task_2/src/App.js
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import 'materialize-css/dist/css/materialize.min.css';
+import './App.css';
+import Navbar from './Components/Navbar'
+import Homepage from './Components/Homepage';
+import InfiniteScrollerWithReactInfiniteScroller from "./Components/Scroll";
+import FloatButton from "./Components/FloatButton";
+class App extends React.Component {
+  
+
+  render() {
+  return (
+    <>
+      <Navbar />
+      <Homepage />
+      <FloatButton />
+        <InfiniteScrollerWithReactInfiniteScroller />
+        <a id="main"/>
+    </>
+  );
+}
+}
+export default App;
